17aae72c9ad3fada9916265ce38d9c47d3146ff7
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-09-04 10:29:25 +0200

    Iteration: Config & middlewares updated

---
+++ API/middlewares/secure.mid.js
@@ -0,0 +1,18 @@
+const createError = require('http-errors');
+
+module.exports.isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        next(createError(401, 'user is not authenticated'))
+    }
+};
+
+module.exports.isUser = (req, res, next) => {
+    if (req.params.id === 'me' || req.user.id == req.params.id) {
+        next();
+    } else {
+        next(createError(403))
+    }
+};
+
